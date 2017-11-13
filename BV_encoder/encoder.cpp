116ac63457d02e27792868f29fc6427e48368f6d
Maja Krivokuca <maja@8i.com>
2017-11-13 14:58:02 +1300

    -Tidying up of initial code.

--- BV_encoder/encoder.cpp
+++ BV_encoder/encoder.cpp
@@ -4,11 +4,10 @@
 #include <iostream>
 #include <algorithm>
 
-//------------------------------------ Function definitions -------------------------------------
+//-------------------------------------------- Function definitions ---------------------------------------------
 
-/*Construct an octree of depth b, based on sorted (ascending order) b-bit Morton codes of
-the input point cloud. This octree will have b + 1 levels in total, from level 0 (root) to
-level b (leaves).*/
+/*Construct an octree of depth b, based on sorted (ascending order) b-bit Morton codes of the input point cloud.
+This octree will have b + 1 levels in total, from level 0 (root) to level b (leaves).*/
 void constructOctree(const int b, octree& my_OT)
 {
 
@@ -16,9 +15,9 @@ void constructOctree(const int b, octree& my_OT)
 	return;
 }
 
-/*For each occupied octree cell at each octree level, compute the x, y, z coordinates of each
-corner of this cell, then extract only the unique corners at each level and the pointer to the
-associated Bezier control point for each unique corner.*/
+/*For each occupied octree cell at each octree level, compute the x, y, z coordinates of each corner of this cell,
+then extract only the unique corners at each level and the pointer to the associated Bezier control point for
+each unique corner.*/
 void computeCornerCoordinates(const int b, const int max_lvl, corners& input_corners, octree& my_OT)
 {
 	for (int lvl = 0; lvl < max_lvl; lvl++)
@@ -38,22 +37,22 @@ void computeCornerCoordinates(const int b, const int max_lvl, corners& input_cor
 			pow(2, (b + 1 - lvl)), 0, pow(2, (b + 1 - lvl)),
 			pow(2, (b + 1 - lvl)), pow(2, (b + 1 - lvl)), pow(2, (b + 1 - lvl)),
 			0, pow(2, (b + 1 - lvl)), pow(2, (b + 1 - lvl));
-		Eigen::Matrix<double, corners1.rows(), 3> offsets_from_origin_rep = ;
+		Eigen::Matrix<double, corners1_rep.rows(), 3> offsets_from_origin_rep = ;
 		Eigen::Matrix<double, offsets_from_origin_rep.rows(), 3> corners2_8 = corners1_rep + offsets_from_origin_rep;	//Corners 2-8 for each octree cell
 
 		/*Reorganize all the corner coordinates for the current octree level, so that all 8 corners for one octree
-		cell have their coordinates listed one after another (i.e, in an 8 x 3 matrix), inside corner_coords*/
+		cell have their coordinates listed one after another (i.e, in an 8 x 3 matrix), inside corner_coords.*/
 		Eigen::Matrix<double, (corners1.rows() + corners2_8.rows()), 3> corner_coords = ;
 
 		//Extract the coordinates of only the unique corners at the current octree level
 		for (int r = 0; r < corner_coords.rows(); r++)
 		{
-			//Get the r-th row of coordinates from corner_coords
+			//Get the r-th row of coordinates
 			Eigen::Array<double, 1, 3> current_row = corner_coords.data.row(r);
 
-			//If the current row has not already been stored inside unique_coords, store it
-			int* p = std::find(input_corners.unique_coords(lvl).row(0), input_corners.unique_coords(lvl).row(1), current_row);
-			if (p != input_corners.unique_coords(lvl).row(1))
+			//If the current row has not already been stored inside unique_coords, store it (otherwise ignore it)
+			int* p = std::find(input_corners.unique_coords(lvl).data.row(0), input_corners.unique_coords(lvl).data.row(), current_row);
+			if (p != input_corners.unique_coords(lvl).row())
 			{
 				input_corners.unique_coords(lvl)() = current_row;
 			}
@@ -67,16 +66,15 @@ void computeCornerCoordinates(const int b, const int max_lvl, corners& input_cor
 	return;
 }
 
-/*For each occupied octree cell at each octree level, extract the occupied voxel coordinates
-associated with that cell, as well as the corresponding normals and centroids if the input
-point cloud has those.*/
+/*For each occupied octree cell at each octree level, extract the occupied voxel coordinates associated with that
+cell, as well as the corresponding normals and centroids if the input point cloud has those.*/
 extractOccupiedVoxels()
 {
 
 }
 
-/*For each unique corner of the occupied blocks at each octree level, compute the Bezier
-control point (SDF value) for this corner.*/
+/*For each unique corner of the occupied blocks at each octree level, compute the Bezier control point (SDF
+value) for this corner.*/
 void computeControlPoints(const int ctrlpt_thresh, const int max_lvl, const int b, corners& input_corners, Eigen::Matrix<Eigen::VectorXd, int depth, 1>& control_points)
 {
 	/*The number of control points inside each vector in control_points will be equal to the number of 
@@ -116,25 +114,24 @@ void computeControlPoints(const int ctrlpt_thresh, const int max_lvl, const int 
 	return;
 }
 
-/*For each octree level after start_lvl, compute the wavelet coefficients associated with
-the unique corners of the occupied blocks at these levels, and compute the reconstructed
-control points at these levels that result after wavelet coefficient quantization and
-dequantization (done to prevent quantization error from accumulating).*/
+/*For each octree level after start_lvl, compute the wavelet coefficients associated with the unique corners of
+the occupied blocks at these levels, and compute the reconstructed control points at these levels that result
+after wavelet coefficient quantization and dequantization (done to prevent quantization error accumulating).*/
 waveletAnalysis()
 {
 
 }
 
-/*Going bottom-up from the voxel level, prune branches (occupancy codes) of octree cells
-that contain all zero (or within the chosen threshold) wavelet coefficients throughout
-the branch. This will result in leaf cells at variable octree levels.*/
+/*Going bottom-up from the voxel level, prune branches (occupancy codes) of octree cells that contain all zero
+(or within the chosen threshold) wavelet coefficients throughout the branch. This will result in leaf cells at
+variable octree levels.*/
 pruneOctree()
 {
 
 }
 
-/*Prune the wavelet coefficient tree to match the pruned octree: wavelet coefficients will
-then exist only for the unpruned occupied octree cells at each level.*/
+/*Prune the wavelet coefficient tree to match the pruned octree: wavelet coefficients will then exist only for
+the unpruned occupied octree cells at each level.*/
 pruneWaveletCoeffTree()
 {
 
@@ -146,11 +143,11 @@ encode()
 
 }
 
-//---------------------------------- Class method definitions -----------------------------------
+//------------------------------------------ Class method definitions -------------------------------------------
 
 
 
-//----------------------------------- Begin program execution -----------------------------------
+//------------------------------------------- Begin program execution -------------------------------------------
 
 int main(int argc, char* argv[])
 {
@@ -183,32 +180,41 @@ int main(int argc, char* argv[])
 	//Read in input point cloud (SVO format)
 	
 
-	/*Construct an octree of depth b, based on sorted (ascending order) b-bit Morton codes of
-	the input point cloud. This octree will have b + 1 levels in total, from level 0 (root) to 
-	level b (leaves).*/
+	/*Construct an octree of depth b, based on sorted (ascending order) b-bit Morton codes of the input point cloud.
+	This octree will have b + 1 levels in total, from level 0 (root) to level b (leaves).*/
 	octree my_OT;
 	constructOctree(b, my_OT);
 
-	/*For each occupied octree cell at each octree level, compute the x, y, z coordinates of each
-	corner of this cell, then extract only the unique corners at each level and the pointer to the
-	associated Bezier control point for each unique corner.*/
+	/*For each occupied octree cell at each octree level, compute the x, y, z coordinates of each corner of this cell,
+	then extract only the unique corners at each level and the pointer to the associated Bezier control point for
+	each unique corner.*/
 	corners input_corners;
 	computeCornerCoordinates(b, max_lvl, input_corners, my_OT);
 
+	/*For each occupied octree cell at each octree level, extract the occupied voxel coordinates associated with that
+	cell, as well as the corresponding normals and centroids if the input point cloud has those.*/
 	extractOccupiedVoxels();
 
-	//Initialize a matrix of (b + 1) vectors (of doubles), to store the control points at each octree level
-	Eigen::Matrix<Eigen::VectorXd, (b + 1), 1> control_points;
-	/*For each unique corner of the occupied blocks at each octree level, compute the Bezier
-	control point (SDF value) for this corner.*/
+	/*For each unique corner of the occupied blocks at each octree level, compute the Bezier control point (SDF
+	value) for this corner.*/
+	Eigen::Matrix<Eigen::VectorXd, (b + 1), 1> control_points;		//Initialize a matrix of (b + 1) vectors (of doubles), to store the control points at each octree level
 	computeControlPoints(ctrlpt_thresh, max_lvl, &input_corners, &control_points);
 
+	/*For each octree level after start_lvl, compute the wavelet coefficients associated with the unique corners of
+	the occupied blocks at these levels, and compute the reconstructed control points at these levels that result
+	after wavelet coefficient quantization and dequantization (done to prevent quantization error accumulating).*/
 	waveletAnalysis();
 
+	/*Going bottom-up from the voxel level, prune branches (occupancy codes) of octree cells that contain all zero
+	(or within the chosen threshold) wavelet coefficients throughout the branch. This will result in leaf cells at
+	variable octree levels.*/
 	pruneOctree();
 
+	/*Prune the wavelet coefficient tree to match the pruned octree: wavelet coefficients will then exist only for
+	the unpruned occupied octree cells at each level.*/
 	pruneWaveletCoeffTree();
 
+	/*Encode the data that will be transmitted to the decoder, and write it to a bitstream.*/
 	encode();
 
 	return 0;
--- BV_encoder/encoder.h
+++ BV_encoder/encoder.h
@@ -1,45 +1,42 @@
 #pragma once
 
-//------------------------------------ Function declarations ------------------------------------
+//-------------------------------------------- Function declarations --------------------------------------------
 
-/*Construct an octree of depth b, based on sorted (ascending order) b-bit Morton codes of
-the input point cloud. This octree will have b + 1 levels in total, from level 0 (root) to
-level b (leaves).*/
+/*Construct an octree of depth b, based on sorted (ascending order) b-bit Morton codes of the input point cloud. 
+This octree will have b + 1 levels in total, from level 0 (root) to level b (leaves).*/
 void constructOctree(const int b, octree& my_OT);
 
-/*For each occupied octree cell at each octree level, compute the x, y, z coordinates of each
-corner of this cell, then extract only the unique corners at each level and the pointer to the
-associated Bezier control point for each unique corner.*/
+/*For each occupied octree cell at each octree level, compute the x, y, z coordinates of each corner of this cell,
+then extract only the unique corners at each level and the pointer to the associated Bezier control point for 
+each unique corner.*/
 void computeCornerCoordinates(const int b, const int max_lvl, corners& input_corners, octree& my_OT);
 
-/*For each occupied octree cell at each octree level, extract the occupied voxel coordinates
-associated with that cell, as well as the corresponding normals and centroids if the input
-point cloud has those.*/
+/*For each occupied octree cell at each octree level, extract the occupied voxel coordinates associated with that 
+cell, as well as the corresponding normals and centroids if the input point cloud has those.*/
 extractOccupiedVoxels();
 
-/*For each unique corner of the occupied blocks at each octree level, compute the Bezier
-control point (SDF value) for this corner.*/
+/*For each unique corner of the occupied blocks at each octree level, compute the Bezier control point (SDF 
+value) for this corner.*/
 void computeControlPoints(const int ctrlpt_thresh, const int max_lvl, const int b, corners& input_corners, Eigen::Matrix<Eigen::VectorXd, int depth, 1>& control_points);
 
-/*For each octree level after start_lvl, compute the wavelet coefficients associated with
-the unique corners of the occupied blocks at these levels, and compute the reconstructed 
-control points at these levels that result after wavelet coefficient quantization and
-dequantization (done to prevent quantization error from accumulating).*/
+/*For each octree level after start_lvl, compute the wavelet coefficients associated with the unique corners of
+the occupied blocks at these levels, and compute the reconstructed control points at these levels that result 
+after wavelet coefficient quantization and dequantization (done to prevent quantization error accumulating).*/
 waveletAnalysis();
 
-/*Going bottom-up from the voxel level, prune branches (occupancy codes) of octree cells
-that contain all zero (or within the chosen threshold) wavelet coefficients throughout
-the branch. This will result in leaf cells at variable octree levels.*/
+/*Going bottom-up from the voxel level, prune branches (occupancy codes) of octree cells that contain all zero 
+(or within the chosen threshold) wavelet coefficients throughout the branch. This will result in leaf cells at 
+variable octree levels.*/
 pruneOctree();
 
-/*Prune the wavelet coefficient tree to match the pruned octree: wavelet coefficients will
-then exist only for the unpruned occupied octree cells at each level.*/
+/*Prune the wavelet coefficient tree to match the pruned octree: wavelet coefficients will then exist only for 
+the unpruned occupied octree cells at each level.*/
 pruneWaveletCoeffTree();
 
 /*Encode the data that will be transmitted to the decoder, and write it to a bitstream.*/
 encode();
 
-//------------------------------------ Structure definitions ------------------------------------
+//-------------------------------------------- Structure definitions --------------------------------------------
 
 struct corners
 {
@@ -57,6 +54,6 @@ struct octree
 
 };
 
-//-------------------------------------- Class definitions --------------------------------------
+//---------------------------------------------- Class definitions ----------------------------------------------
 
 
